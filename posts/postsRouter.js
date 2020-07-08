8c984840cf62fa7d9178e11382dd8e171f2bb256
Anthony Carrillo <carrilloanthony2698@gmail.com>
2020-07-08 13:27:39 -0400

    Added two get req.

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -3,4 +3,27 @@ const db = require("../data/db");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    db.find()
+        .then(response => {
+            res.status(200).json({ message: response })
+        })
+        .catch(error => {
+            res.status(500).json({ errorMessage: "The posts information could not be retrieved." })
+        });
+});
+
+router.get("/:id", (req, res) => {
+    db.findById(req.params.id)
+        .then(response => {
+            if ( response.length === 0 ) {
+                res.status(404).json({ errorMessage: "The post with the specified ID does not exist." });
+            } else if ( response ) {
+                res.status(200).json({ message: response });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ errorMessage: "The posts information could not be retrieved." });
+        });
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
+const postsRouter = require("./posts/postsRouter");
+
 const server = express();
 
 server.use(express.json());
 
+server.use("/api/posts", postsRouter);
 
 server.get("/", (req, res) => {
     res.status(200).json({ API: "Running...." })
